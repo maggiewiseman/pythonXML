b68c7f3d1554bb440f73e988734f87bbd8e8426e
maggiewiseman <maggie.wiseman@gmail.com>
2017-12-10 17:00:23 -0500

    reading in a csv file and turning it into xml

--- csvToXML.py
+++ csvToXML.py
@@ -1,24 +1,35 @@
 import csv
-
-def invoke():
-    with open('movies.csv') as csvfile:
-        myreader = csv.reader(csvfile, delimiter=',')
-        for row in myreader:
-            print '!'.join(row)
-
-
-def makeXML():
-    f = open('movies.csv')
-    csv_f = csv.reader(f)
-    data = []
-
-    for row in csv_f:
-        data.append(row)
-    f.close()
-
-    print data[1:]
+#
+# def invoke():
+#     with open('movies.csv') as csvfile:
+#         myreader = csv.reader(csvfile, delimiter=',')
+#         for row in myreader:
+#             print '!'.join(row)
+#
+#
+# def makeXML():
+#     f = open('movies.csv')
+#     csv_f = csv.reader(f)
+#     data = []
+#
+#     for row in csv_f:
+#         data.append(row)
+#     f.close()
+#
+#     print data[1:]
     # loop through data
     # convert_row
+
+
+f = open('movies.csv')
+csv_f = csv.reader(f)
+data = []
+
+for row in csv_f:
+    data.append(row)
+f.close()
+
+print data[1:]
 
 def convert_row(row):
     return """<movietitle="%s">
@@ -31,3 +42,4 @@ def convert_row(row):
 </movie>"""%(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
 
 print '\n'.join([convert_row(row) for row in data[1:]])
+
